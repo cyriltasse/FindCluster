6eef81e90d2d3167425238b5b23568143c7174f1
cyriltasse <cyril.tasse@yahoo.fr>
2019-09-30 11:00:29 +0200

    ok

---
+++ ClassSimulCatalog.py
@@ -0,0 +1,11 @@
+import numpy as np
+import ClassGeneDist
+
+
+class ClassSimulCatalog():
+    def __init__(self,N=10000):
+        
+        L=np.
+        DM=ClassGeneDist.ClassDistMachine()
+        DM.setLuminosityFunction(X,W)
+        DM.GiveSample(1000)
